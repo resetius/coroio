3965ac65615d80e6a684b9ea472186157ce94d60
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-04-02 00:53:14 +0200

    ignore sigpipe

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
+#include <signal.h>
 
 #include <net.hpp>
 
@@ -35,21 +36,21 @@ void test_accept(void**) {
     socket.Bind();
     socket.Listen();
 
-    TSimpleTask h1 = [](TLoop* loop) -> TSimpleTask 
+    TSimpleTask h1 = [](TLoop* loop) -> TSimpleTask
     {
         TSocket client(TAddress{"127.0.0.1", 8888}, loop);
         co_await client.Connect();
         co_return;
     }(&loop);
 
-    TSimpleTask h2 = [](TSocket* socket, TSocket* clientSocket) -> TSimpleTask 
+    TSimpleTask h2 = [](TSocket* socket, TSocket* clientSocket) -> TSimpleTask
     {
         *clientSocket = std::move(co_await socket->Accept());
         co_return;
     }(&socket, &clientSocket);
 
     loop.OneStep();
-    loop.HandleEvents(); 
+    loop.HandleEvents();
 
     in_addr addr1 = clientSocket.Addr().Addr().sin_addr;
     in_addr addr2 = socket.Addr().Addr().sin_addr;
@@ -60,21 +61,21 @@ void test_connection_refused_on_write(void**) {
     TLoop loop;
     int err = 0;
 
-    TSimpleTask h1 = [](TLoop* loop, int* err) -> TSimpleTask 
+    TSimpleTask h1 = [](TLoop* loop, int* err) -> TSimpleTask
     {
         TSocket clientSocket(TAddress{"127.0.0.1", 8888}, loop);
         char buffer[] = "test";
         try {
             co_await clientSocket.Connect();
             co_await clientSocket.WriteSome(buffer, sizeof(buffer));
-        } catch (const TSystemError& ex) { 
+        } catch (const TSystemError& ex) {
             *err = ex.Errno();
         }
         co_return;
     }(&loop, &err);
 
     loop.OneStep();
-    loop.HandleEvents(); 
+    loop.HandleEvents();
 
     assert_int_equal(err, ECONNREFUSED);
 }
@@ -83,21 +84,21 @@ void test_connection_refused_on_read(void**) {
     TLoop loop;
     int err = 0;
 
-    TSimpleTask h2 = [](TLoop* loop, int* err) -> TSimpleTask 
+    TSimpleTask h2 = [](TLoop* loop, int* err) -> TSimpleTask
     {
         TSocket clientSocket(TAddress{"127.0.0.1", 8888}, loop);
         char buffer[] = "test";
         try {
             co_await clientSocket.Connect();
             co_await clientSocket.ReadSome(buffer, sizeof(buffer));
-        } catch (const TSystemError& ex) { 
+        } catch (const TSystemError& ex) {
             *err = ex.Errno();
         }
         co_return;
     }(&loop, &err);
 
     loop.OneStep();
-    loop.HandleEvents(); 
+    loop.HandleEvents();
 
     assert_int_equal(err, ECONNREFUSED);
 }
@@ -107,7 +108,7 @@ void test_timeout(void**) {
     auto now = std::chrono::steady_clock::now();
     auto timeout = std::chrono::milliseconds(100);
     TEvent::TTime next;
-    TSimpleTask h = [](TLoop* loop, TEvent::TTime* next, std::chrono::milliseconds timeout) -> TSimpleTask 
+    TSimpleTask h = [](TLoop* loop, TEvent::TTime* next, std::chrono::milliseconds timeout) -> TSimpleTask
     {
         co_await loop->Sleep(timeout);
         *next = std::chrono::steady_clock::now();
@@ -119,6 +120,7 @@ void test_timeout(void**) {
 }
 
 int main() {
+    signal(SIGPIPE, SIG_IGN);
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_addr),
         cmocka_unit_test(test_listen),
