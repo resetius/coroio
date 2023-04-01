9d9148b2137d1185e06926a53c4cf13b782bf1cc
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-04-01 23:12:53 +0200

    Simplify

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -4,25 +4,17 @@
 using namespace NNet;
 
 TSimpleTask client_handler(TSocket&& s, TLoop* loop ) {
-    char buffer[1024] = {0};
+    char buffer[128] = {0};
     TSocket socket(std::move(s));
 
     while (true) {
         buffer[0] = '\0';
         auto size = co_await socket.ReadSome(buffer, sizeof(buffer));
-        if (size <= 0) {
-            std::cerr << "Connection closed " << size << "\n";
-        }
-
-        if (size > 0) {
-            std::cerr << "Received from client: " << buffer << "\n";
-        }
+        buffer[size] = 0;
+        std::cerr << "Received from client: " << buffer << "\n";
 
         co_await socket.WriteSome(buffer, sizeof(buffer));
-        //co_await loop->Sleep(std::chrono::milliseconds(10));
     }
-
-    std::cerr << "Return\n";
 
     co_return;
 }
@@ -33,42 +25,46 @@ TSimpleTask server(TLoop* loop)
     TSocket socket(std::move(address), loop);
     socket.Bind();
     socket.Listen();
-    int i = 1;
 
     while (true) {
         auto client = co_await socket.Accept();
-        std::cerr << "Accepted " << i++ << "\n";
-        client_handler(std::move(client), loop); // TODO: destroy coro on client disconnect
-        // co_await loop->Sleep(std::chrono::milliseconds(1000));
+        client_handler(std::move(client), loop);
     }
+    co_return;
 }
 
-TSimpleTask client(TLoop* loop)
+TSimpleTask client(TLoop* loop, int clientId)
 {
     TAddress address("127.0.0.1", 8888);
-    char buffer[] = "Hello\n\0";
+    char buffer[128] = "Hello XXX/YYY";
     char rcv[1024] = {0};
+    int messageNo = 1;
 
     TSocket socket(address, loop);
     co_await socket.Connect();
-    std::cerr << "Connected\n";
+
     while (true) {
-        auto size = co_await socket.WriteSome(buffer, sizeof(buffer));
-        //std::cerr << "Bytes written : " << size << "\n";
+        snprintf(buffer+6, sizeof(buffer)-6, "%03d/%03d", messageNo++, clientId);
+        auto size = co_await socket.WriteSome(buffer, strlen(buffer));
         size = co_await socket.ReadSome(rcv, sizeof(rcv));
         std::cerr << "Received from server: " << rcv << std::endl;
         co_await loop->Sleep(std::chrono::milliseconds(1000));
     }
-    std::cerr << "Return\n";
+    co_return;
 }
 
-int main() {
+int main(int argc, char** argv) {
     signal(SIGPIPE, SIG_IGN);
 
     TLoop loop;
+    int clients = 0;
+    if (argc > 1) { clients = atoi(argv[1]); }
+    if (clients == 0) { clients = 1; }
 
     server(&loop);
-    client(&loop);
+    for (int i = 0; i < clients; i++) {
+        client(&loop, i+1);
+    }
 
     loop.Loop();
 }
