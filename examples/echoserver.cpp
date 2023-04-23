eeee7ac86a79a165e888d5079f78acbb8b31439f
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-04-23 22:33:10 +0200

    Can select any method

--- examples/echoserver.cpp
+++ examples/echoserver.cpp
@@ -10,15 +10,25 @@
 #include <uring.hpp>
 #endif
 
-using NNet::TSimpleTask;
-using NNet::TSocket;
-using NNet::TSelect;
-using NNet::TEPoll;
-using NNet::TAddress;
-using NNet::TUring;
-using TLoop = NNet::TLoop<TEPoll>;
+#if defined(__APPLE__) || defined(__FreeBSD__)
+#include <kqueue.hpp>
+#endif
 
-template<bool debug>
+using NNet::TSimpleTask;
+using NNet::TAddress;
+using NNet::TSelect;
+using NNet::TPoll;
+
+#ifdef __linux__
+using NNet::TEPoll;
+using NNet::TUring;
+#endif
+
+#if defined(__APPLE__) || defined(__FreeBSD__)
+using NNet::TKqueue;
+#endif
+
+template<bool debug, typename TSocket>
 TSimpleTask client_handler(TSocket socket, int buffer_size) {
     std::vector<char> buffer(buffer_size); ssize_t size = 0;
 
@@ -38,10 +48,10 @@ TSimpleTask client_handler(TSocket socket, int buffer_size) {
     co_return;
 }
 
-template<bool debug>
-TSimpleTask server(TLoop* loop, TAddress address, int buffer_size)
+template<bool debug, typename TPoller>
+TSimpleTask server(TPoller& poller, TAddress address, int buffer_size)
 {
-    TSocket socket(std::move(address), loop->Poller());
+    typename TPoller::TSocket socket(std::move(address), poller);
     socket.Bind();
     socket.Listen();
 
@@ -52,51 +62,29 @@ TSimpleTask server(TLoop* loop, TAddress address, int buffer_size)
     co_return;
 }
 
-template<bool debug>
-TSimpleTask client_handler_ur(NNet::TUringSocket socket, int buffer_size) {
-    std::vector<char> buffer(buffer_size); ssize_t size = 0;
-
-    try {
-        while ((size = co_await socket.ReadSome(buffer.data(), buffer_size)) > 0) {
-            if constexpr(debug) {
-                std::cerr << "Received: " << std::string_view(buffer.data(), size) << "\n";
-            }
-            co_await socket.WriteSome(buffer.data(), size);
-        }
-    } catch (const std::exception& ex) {
-        std::cerr << "Exception: " << ex.what() << "\n";
-    }
-    if (size == 0) {
-        std::cerr << "Client disconnected\n";
-    }
-    co_return;
-}
-
-template<bool debug>
-TSimpleTask server_ur(TUring* uring, TAddress address, int buffer_size)
+template<typename TPoller>
+void run(bool debug, TAddress address, int buffer_size)
 {
-    NNet::TUringSocket socket(std::move(address), *uring);
-    socket.Bind();
-    socket.Listen();
-
-    while (true) {
-        auto client = co_await socket.Accept();
-        client_handler_ur<debug>(std::move(client), buffer_size);
+    NNet::TLoop<TPoller> loop;
+    if (debug) {
+        server<true>(loop.Poller(), std::move(address), buffer_size);
+    } else {
+        server<false>(loop.Poller(), std::move(address), buffer_size);
     }
-    co_return;
+    loop.Loop();
 }
 
 int main(int argc, char** argv) {
     signal(SIGPIPE, SIG_IGN);
     int port = 0;
     int buffer_size = 128;
-    bool uring = false;
+    std::string method = "select";
     bool debug = false;
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "--port") && i < argc-1) {
             port = atoi(argv[++i]);
-        } else if (!strcmp(argv[i], "--uring")) {
-            uring = true;
+        } else if (!strcmp(argv[i], "--method") && i < argc-1) {
+            method = argv[++i];
         } else if (!strcmp(argv[i], "--debug")) {
             debug = true;
         } else if (!strcmp(argv[i], "--buffer-size") && i < argc-1) {
@@ -107,26 +95,28 @@ int main(int argc, char** argv) {
     if (buffer_size == 0) { buffer_size = 128; }
 
     TAddress address{"0.0.0.0", port};
+    std::cerr << "Method: " << method << "\n";
+
+    if (method == "select") {
+        run<TSelect>(debug, address, buffer_size);
+    } else if (method == "poll") {
+        run<TPoll>(debug, address, buffer_size);
+    }
 #ifdef __linux__
-    if (uring) {
-        std::cout << "Using uring \n";
-        NNet::TLoop<NNet::TUring> loop;
-        if (debug) {
-            server_ur<true>(&loop.Poller(), std::move(address), buffer_size);
-        } else {
-            server_ur<false>(&loop.Poller(), std::move(address), buffer_size);
-        }
-        loop.Loop();
-    } else
+    else if (method == "epoll") {
+        run<TEPoll>(debug, address, buffer_size);
+    }
+    else if (method == "uring") {
+        run<TUring>(debug, address, buffer_size);
+    }
 #endif
-    {
-        TLoop loop;
-        if (debug) {
-            server<true>(&loop, std::move(address), buffer_size);
-        } else {
-            server<false>(&loop, std::move(address), buffer_size);
-        }
-        loop.Loop();
+#if defined(__APPLE__) || defined(__FreeBSD__)
+    else if (method == "kqueue") {
+        run<TKqueue>(debug, address, buffer_size);
+    }
+#endif
+    else {
+        std::cerr << "Unknown method\n";
     }
     return 0;
 }
