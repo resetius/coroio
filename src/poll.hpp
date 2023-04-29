ce23fdfbf4f1e8e04efdfdad3881c81795bed1b9
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-04-29 23:15:11 +0200

    Use new changelist

--- src/poll.hpp
+++ src/poll.hpp
@@ -19,42 +19,60 @@ public:
         auto deadline = Timers_.empty() ? TTime::max() : Timers_.top().Deadline;
         int timeout = GetMillis(TClock::now(), deadline);
 
-        for (auto& [k, ev] : Events_) {
-            if (InEvents_.size() <= k) {
-                InEvents_.resize(k+1, std::make_tuple(TEvent{}, -1));
-            }
-            auto& [old_ev, idx] = InEvents_[k];
-            pollfd pev = {.fd = k, .events = 0, .revents = 0};
-            if (ev.Read) {
-                pev.events |= POLLIN;
-            }
-            if (ev.Write) {
-                pev.events |= POLLOUT;
-            }
-
-            if (pev.events) {
-                if (idx == -1) {
-                    idx = Fds_.size();
-                    Fds_.emplace_back(std::move(pev));
-                } else {
-                    Fds_[idx] = pev;
-                }
-                old_ev = ev;
-            } else {
-                if (idx != -1) {
-                    std::swap(Fds_[idx], Fds_.back());
-                    std::get<1>(InEvents_[Fds_[idx].fd]) = idx;
-                    Fds_.back() = {};
-                }
-
-                while (!Fds_.empty() && !Fds_.back().events) {
-                    Fds_.pop_back();
-                }
-            }
+        if (InEvents_.size() <= MaxFd_) {
+            InEvents_.resize(MaxFd_+1, std::make_tuple(TEvent{}, -1));
         }
 
-        Events_.clear();
-        ReadyHandles_.clear();
+        auto dropFlag = [&]<int flag>(std::queue<int>& q) {
+            while (!q.empty()) {
+                auto k = q.front(); q.pop();
+                auto& [ev, idx] = InEvents_[k];
+                if (idx != -1) {
+                    Fds_[idx].events &= ~flag;
+                    if (Fds_[idx].events == 0) {
+                        std::swap(Fds_[idx], Fds_.back());
+                        std::get<1>(InEvents_[Fds_[idx].fd]) = idx;
+                        Fds_.back() = {};
+                    }
+
+                    if constexpr(flag == POLLIN) {
+                        ev.Read = {};
+                    } else {
+                        ev.Write = {};
+                    }
+                }
+            }
+        };
+
+        auto addFlag = [&]<int flag>(std::vector<std::tuple<int,THandle>>& q) {
+            for (auto& [k, h] : q) {
+                auto& [ev, idx] = InEvents_[k];
+                if (idx == -1) {
+                    idx = Fds_.size();
+                    Fds_.emplace_back(pollfd{});
+                }
+
+                pollfd& pev = Fds_[idx];
+                pev.fd = k;
+                pev.events |= flag;
+                if constexpr(flag == POLLIN) {
+                    ev.Read = h;
+                } else {
+                    ev.Write = h;
+                }
+            }
+        };
+
+        dropFlag.template operator()<POLLIN>(DelReads_);
+        dropFlag.template operator()<POLLOUT>(DelWrites_);
+        addFlag.template operator()<POLLIN>(NewReads_);
+        addFlag.template operator()<POLLOUT>(NewWrites_);
+
+        while (!Fds_.empty() && !Fds_.back().events) {
+            Fds_.pop_back();
+        }
+
+        Reset();
 
         if (poll(&Fds_[0], Fds_.size(), timeout) < 0) {
             throw std::system_error(errno, std::generic_category(), "poll");
@@ -62,31 +80,27 @@ public:
 
         for (auto& pev : Fds_) {
             auto& [ev, _] = InEvents_[pev.fd];
-            bool changed = false;
             if (pev.revents & POLLIN) {
                 ReadyHandles_.emplace_back(std::move(ev.Read));
                 ev.Read = {};
-                changed |= true;
+                DelReads_.emplace(pev.fd);
             }
             if (pev.revents & POLLOUT) {
                 ReadyHandles_.emplace_back(std::move(ev.Write));
                 ev.Write = {};
-                changed |= true;
+                DelWrites_.emplace(pev.fd);
             }
             if (pev.revents & POLLHUP) {
-                changed |= true;
                 if (ev.Read) {
                     ReadyHandles_.emplace_back(std::move(ev.Read));
                     ev.Read = {};
+                    DelReads_.emplace(pev.fd);
                 }
                 if (ev.Write) {
                     ReadyHandles_.emplace_back(std::move(ev.Write));
                     ev.Write = {};
+                    DelWrites_.emplace(pev.fd);
                 }
-            }
-
-            if (changed) {
-                Events_.emplace(pev.fd, ev);
             }
         }
 
