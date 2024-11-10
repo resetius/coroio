2a50625d86342aa5ba1a2113cff2d09d2b8066c3
Alexey Ozeritskiy <aozeritsky@gmail.com>
2024-11-10 19:22:48 +0000

    Work on Win32 support (select) (#45)

--- coroio/select.hpp
+++ coroio/select.hpp
@@ -20,17 +20,34 @@ public:
 
     void Poll();
 
+#ifdef _WIN32
+    TSelect();
+#endif
+
 private:
     fd_set* ReadFds() {
+#ifdef _WIN32
+        return &ReadFds_;
+#else
         return reinterpret_cast<fd_set*>(&ReadFds_[0]);
+#endif
     }
     fd_set* WriteFds() {
+#ifdef _WIN32
+        return &WriteFds_;
+#else
         return reinterpret_cast<fd_set*>(&WriteFds_[0]);
+#endif
     }
 
     std::vector<THandlePair> InEvents_;
+#ifdef _WIN32
+    fd_set ReadFds_;
+    fd_set WriteFds_;
+#else
     std::vector<fd_mask> ReadFds_;
     std::vector<fd_mask> WriteFds_;
+#endif
 };
 
 } // namespace NNet
