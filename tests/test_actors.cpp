d68a83d4b2454f8e90a453738686790fea45fb65
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-07-26 21:05:53 +0100

    Add tests

--- tests/test_actors.cpp
+++ tests/test_actors.cpp
@@ -10,6 +10,7 @@
 
 #include <coroio/all.hpp>
 #include <coroio/actors/actorsystem.hpp>
+#include <coroio/actors/messages.hpp>
 
 extern "C" {
 #include <cmocka.h>
@@ -142,10 +143,89 @@ void test_ask_respond(void**) {
     }
 }
 
+struct TPodMessage {
+    int field1;
+    double field2;
+    char field3;
+    double field4;
+    double field5;
+};
+
+struct TAllocator {
+    void* Acquire(size_t size) {
+        return ::operator new(size);
+    }
+
+    void Release(void* ptr) {
+        ::operator delete(ptr);
+    }
+};
+
+void test_serialize_pod(void**) {
+    TPodMessage msg{1, 2.0, 'c', 4.0, 5.0};
+    TAllocator allocator;
+
+    // Serialize
+    auto blob = NNet::NActors::SerializeNear(std::move(msg), allocator);
+    assert_true(blob.IsPOD);
+
+    // Deserialize
+    auto& deserialized = NNet::NActors::DeserializeNear<TPodMessage>(blob);
+    assert_true(deserialized.field1 == 1);
+    assert_true(deserialized.field2 == 2.0);
+    assert_true(deserialized.field3 == 'c');
+    assert_true(deserialized.field4 == 4.0);
+    assert_true(deserialized.field5 == 5.0);
+
+    auto farBlob = NNet::NActors::SerializeFar<TPodMessage>(blob, allocator);
+    assert_true(farBlob.Data.get() == blob.Data.get());
+
+    auto& deserializedFar = NNet::NActors::DeserializeFar<TPodMessage>(farBlob);
+    assert_true(deserializedFar.field1 == 1);
+    assert_true(deserializedFar.field2 == 2.0);
+    assert_true(deserializedFar.field3 == 'c');
+    assert_true(deserializedFar.field4 == 4.0);
+    assert_true(deserializedFar.field5 == 5.0);
+}
+
+namespace NNet::NActors {
+
+template<>
+void SerializeToStream<std::string>(const std::string& obj, std::ostringstream& oss) {
+    oss << obj;
+}
+
+template<>
+void DeserializeFromStream<std::string>(std::string& obj, std::istringstream& iss) {
+    obj = iss.str();
+}
+
+} // namespace NNet::NActors
+
+void test_serialize_non_pod(void**) {
+    TAllocator allocator;
+    std::string str = "Hello, World!";
+    auto size = str.size();
+    NNet::NActors::TBlob blob = NNet::NActors::SerializeNear<std::string>(std::move(str), allocator);
+    assert_true(blob.IsPOD == false);
+
+    std::string& deserialized = NNet::NActors::DeserializeNear<std::string>(blob);
+    assert_string_equal(deserialized.c_str(), "Hello, World!");
+
+    auto farBlob = NNet::NActors::SerializeFar<std::string>(blob, allocator);
+    assert_true(farBlob.Data.get() != blob.Data.get());
+    assert_true(farBlob.Size == size);
+
+    std::string deserializedFar = NNet::NActors::DeserializeFar<std::string>(farBlob);
+    assert_string_equal(deserializedFar.c_str(), "Hello, World!");
+}
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_ping_pong),
         cmocka_unit_test(test_ask_respond),
+        cmocka_unit_test(test_serialize_pod),
+        cmocka_unit_test(test_serialize_non_pod),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
