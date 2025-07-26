fd19d58a5c2fc0e568d0f41e9154beb4055cee22
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-07-26 20:09:34 +0100

    Work on actors messages serialization

---
+++ coroio/actors/messages.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <memory>
+#include <sstream>
+#include <cstring>
+#include <type_traits>
+
+namespace NNet {
+namespace NActors {
+
+struct TBlob {
+    using TRawPtr = std::shared_ptr<void>;
+    TRawPtr Data;
+    uint32_t Size;
+    bool IsPOD;
+    enum class PointerType {
+        Near,  // Pointer to the object (for actor communication)
+        Far    // Serialized representation (for network transmission)
+    } Type;
+};
+
+template<typename T>
+constexpr bool is_pod_v = std::is_trivially_copyable_v<T> && std::is_standard_layout_v<T>;
+
+template<typename T, typename TAllocator>
+typename std::enable_if_t<is_pod_v<T>, TBlob>
+SerializePodNear(T&& message, TAllocator& alloc)
+{
+    uint32_t size = sizeof(T);
+    auto* data = alloc.acquire(size);
+    new (data) T(std::move(message));
+
+    auto rawPtr = TBlob::TRawPtr(data, [alloc](void* ptr) {
+        alloc.release(ptr);
+    });
+
+    return TBlob{std::move(rawPtr), size, true, TBlob::PointerType::Near};
+}
+
+template<typename T, typename TAllocator>
+typename std::enable_if_t<!is_pod_v<T>, TBlob>
+SerializeNonPodNear(T&& message, TAllocator& alloc)
+{
+    // For non-POD types, we use regular memory allocation
+    T* obj = new T(std::move(message));
+
+    auto rawPtr = TBlob::TRawPtr(obj, [](void* ptr) {
+        delete reinterpret_cast<T*>(ptr);
+    });
+
+    return TBlob{std::move(rawPtr), sizeof(T), false, TBlob::PointerType::Near};
+}
+
+template<typename T, typename TAllocator>
+TBlob SerializeNear(T&& message, TAllocator& alloc)
+{
+    if constexpr (is_pod_v<T>) {
+        return SerializePodNear(std::forward<T>(message), alloc);
+    } else {
+        return SerializeNonPodNear(std::forward<T>(message), alloc);
+    }
+}
+
+template<typename T, typename TAllocator>
+TBlob SerializeFar(TBlob blob, TAllocator& alloc)
+{
+    if constexpr (is_pod_v<T>) {
+        blob.Type = TBlob::PointerType::Far;
+        return blob; // For POD, far == near, just share the pointer
+    } else {
+        T* obj = reinterpret_cast<T*>(blob.Data.get());
+        std::ostringstream oss;
+        SerializeToStream(*obj, oss);
+        void* data = ::operator new(oss.str().size());
+        std::memcpy(data, oss.str().data(), oss.str().size());
+        auto rawPtr = TBlob::TRawPtr(data, [alloc](void* ptr) {
+            ::operator delete(ptr);
+        });
+        return TBlob{std::move(rawPtr), static_cast<uint32_t>(oss.str().size()), false, TBlob::PointerType::Far};
+    }
+}
+
+template<typename T>
+void SerializeToStream(const T& obj, std::ostringstream& oss)
+{
+    static_assert(sizeof(T) == 0, "Serialization not implemented for this type");
+}
+
+template<typename T>
+T& DeserializeNear(const TBlob& blob) {
+    return *reinterpret_cast<T*>(blob.Data.get());
+}
+
+template<typename T>
+auto DeserializeFar(const TBlob& blob) -> std::conditional_t<is_pod_v<T>, T&, T> {
+    if constexpr (is_pod_v<T>) {
+        return *reinterpret_cast<T*>(blob.Data.get());
+    } else {
+        std::istringstream iss(std::string(reinterpret_cast<const char*>(blob.Data.get()), blob.Size));
+        T obj;
+        DeserializeFromStream(obj, iss);
+        return obj;
+    }
+}
+
+} // namespace NActors
+} // namespace NNet
