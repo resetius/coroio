515e70ed037ae831d605673dd578b242297be699
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-08-09 17:34:46 +0100

    Improve envelope reader

--- coroio/actors/envelope_reader.hpp
+++ coroio/actors/envelope_reader.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "actor.hpp"
+#include "queue.hpp"
 
 #include <deque>
+#include <span>
 
 namespace NNet {
 namespace NActors {
@@ -33,6 +35,35 @@ private:
     THeader Header;
     std::queue<TEnvelope> Messages;
     std::deque<char> Buffer;
+};
+
+class TZeroCopyEnvelopeReader {
+public:
+    // capacity must be a power of two
+    TZeroCopyEnvelopeReader(size_t capacity = 1024);
+    std::span<char> Acquire(size_t size);
+    void Commit(size_t size);
+    std::optional<TEnvelope> Pop();
+    size_t Size() const;
+    bool NeedMoreData() const {
+        return !HasHeader || Size() < Header.Size;
+    }
+
+    // for testing purposes
+    void Push(const char* p, size_t len);
+
+private:
+    void EnsureCapacity(size_t size);
+    void CopyOut(char* buf, size_t size);
+    void TryReadHeader();
+
+    std::vector<char> Data;
+    size_t Head = 0;
+    size_t Tail = 0;
+    size_t LastIndex = 0;
+
+    bool HasHeader = false;
+    THeader Header;
 };
 
 } // namespace NActors
