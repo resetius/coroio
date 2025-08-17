965bed5e98f848b3ddadb93203e04084c7760fba
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-08-18 00:45:24 +0100

    Get rid of std::function

--- coroio/actors/messages_factory.hpp
+++ coroio/actors/messages_factory.hpp
@@ -3,7 +3,6 @@
 #include "messages.hpp"
 
 #include <vector>
-#include <functional>
 
 namespace NNet {
 namespace NActors {
@@ -16,10 +15,10 @@ public:
         if (Handlers_.size() <= messageId) {
             Handlers_.resize(messageId + 1);
         }
-        auto serializeFunc = [](TBlob&& blob) {
+        auto serializeFunc = +[](TBlob&& blob) {
             return ::NNet::NActors::SerializeFar<T>(std::move(blob));
         };
-        Handlers_[messageId] = std::move(serializeFunc);
+        Handlers_[messageId] = serializeFunc;
     }
 
     TBlob SerializeFar(uint32_t messageId, TBlob blob) {
@@ -32,11 +31,7 @@ public:
     }
 
 private:
-#if __cpp_lib_move_only_function >= 202110L
-    std::vector<std::move_only_function<TBlob(TBlob&&)>> Handlers_;
-#else
-    std::vector<std::function<TBlob(TBlob&&)>> Handlers_;
-#endif
+    std::vector<TBlob(*)(TBlob&&)> Handlers_;
 };
 
 } // namespace NActors
